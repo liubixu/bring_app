1fa9533614a12fb4ef3d812694732409adbef872
ethan <liubixu@gmail.com>
2023-03-06 02:13:05 +1300

    update register page, add logo, title, email and password controls.

--- lib/Login.dart
+++ lib/Login.dart
@@ -12,6 +12,7 @@ class Login extends StatelessWidget {
         body: Column(
           children: [
             Container(
+              alignment: Alignment.centerLeft,
               margin: const EdgeInsets.only(top: 46.0, left: 15),
               child: InkWell(
                 onTap: () {
@@ -25,19 +26,66 @@ class Login extends StatelessWidget {
                 ),
               ),
             ),
-            TextField(
-              onChanged: (value) {
-                // Handle input text changes
-              },
-              decoration: const InputDecoration(
-                labelText: 'Enter your name',
-                hintText: 'e.g. John Doe',
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(),
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 16, horizontal: 12),
-                isDense: true,
-                labelStyle: TextStyle(color: Colors.grey),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(top: 13.0),
+              child: Image.asset(
+                'assets/images/bg_logo.png',
+                width: 155,
+                height: 49,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 17.0),
+              child: const Text(
+                'Welcome back',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 110.0, left: 15, right: 15),
+              child: const Text(
+                'Email Address',
+                style: TextStyle(fontSize: 14.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(top: 13.0, left: 15, right: 15),
+              child: TextField(
+                onChanged: (value) {
+                  // Handle input text changes
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Email Address',
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 16, horizontal: 12),
+                  isDense: true,
+                  labelStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 28.0, left: 15, right: 15),
+              child: const Text(
+                'Password',
+                style: TextStyle(fontSize: 14.0),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.only(top: 13.0, left: 15, right: 15),
+              child: const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
               ),
             ),
           ],
